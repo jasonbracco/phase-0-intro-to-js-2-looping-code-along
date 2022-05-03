df96b077a7eed5899181f06d1bf11ff15a8a8ef6
Jason Bracco <bracco.jason@gmail.com>
2022-05-03 19:02:01 -0400

    commit tree 189e88809a67b7442d91f3e0ad59d40d40f3d998

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(array, eventName){
+    let thankYouCards = []
+    for(let i = 0; i < array.length; i++){ 
+        console.log(thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`))
+        debugger
+    }
+     return thankYouCards
+}
+
+let i
+function countDown(i){
+    while (i>=0){
+    console.log(i)
+    i--
+}
+}
